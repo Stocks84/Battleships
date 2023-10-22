ff21780e5bff82ebed091162f29694cb7defe8e1
William Stocks <willstocks@me.com>
2023-10-22 10:33:27 +0000

    Update board six

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 board = []
 
-for _ in range(0, 10):
-    board.append(['0'] * 10)
+for _ in range(0, 8):
+    board.append(['O'] * 8)
 
 # print(board)
 
 def print_board(board):
     """
-    This is for the asthetics of the game.
+    This is for the aesthetics of the game.
     Making nice rows for the user.
     """
     for row in board:
-        print(row)
+        print(' '.join(row))
 
 print_board(board)
