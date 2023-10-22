4130fbf31cfd80a790b0ef00e5a5b611572055fe
William Stocks <willstocks@me.com>
2023-10-22 09:57:20 +0000

    Add game board and rows for print_board

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
-Battleship
+board = []
+
+for _ in range(0, 10):
+    board.append(['0'] * 10)
+
+# print(board)
+
+def print_board(board):
+    """
+    This is for the asthetics of the game.
+    Making nice rows for the user.
+    """
+    for row in board:
+        print(row)
+
+print_board(board)
