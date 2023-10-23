92021f775d3b91871d7f383042178e2dc9092b9a
William Stocks <willstocks@me.com>
2023-10-23 09:21:14 +0000

    Add land_guess function

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from random import randint
 board = []
 
 for x in range(0, 8):
-    board.append(['O'] * 8)
+    board.append(['o'] * 8)
 
 
 def print_board(board):
@@ -35,6 +35,7 @@ def random_col(board):
 
 bship_row = random_row(board)
 bship_col = random_col(board)
+# Delete these prints after testing and undo comment down below
 print(bship_row)
 print(bship_col)
 guess_row = int(input("Guess Row: "))
@@ -50,10 +51,21 @@ def correct_guess():
     """
     if guess_row == bship_row and guess_col == bship_col:
         print("BOOM!!! That was a Great hit!\n")
-    else:
-        print("Ah you missed!!! You need to improve your aim!!!\n")
-        board[guess_row][guess_col] = 'X'
-        print_board(board)
+    # else:
+    #     print("Ah you missed!!! You need to improve your aim!!!\n")
+    #     board[guess_row][guess_col] = 'X'
+    #     print_board(board)
 
 
 correct_guess()
+
+# Might need to be before correct guess function
+def land_guess():
+    """
+    When player guesses out of range of the board a message will appear.
+    """
+    if guess_row not in range(8) or guess_col not in range(8):
+        print("Oh no, you have hit DRY LAND!!! Please try again!!")
+        print("Anywhere between 1 & 8 \n")
+
+land_guess()
